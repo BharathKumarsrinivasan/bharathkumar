e8d335e2d709be3a605f28a141e3360100334bcf
BharathKumarsrinivasan <50615985+BharathKumarsrinivasan@users.noreply.github.com>
2019-05-14 14:38:32 +0530

    Update checksum.c

--- checksum.c
+++ checksum.c
@@ -4,10 +4,10 @@ int main(void) {
 	int n;
 	scanf("%d",&n);
 	if(n>0)
-	printf("positive");
+	printf("Positive");
 	else if(n<0)
-	printf("negative");
+	printf("Negative");
 	else
-	printf("zero");
+	printf("Zero");
 	return 0;
 }
